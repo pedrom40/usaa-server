86781d2985c4e05442506c6d3dce0a184b7fba2e
pedrom40 <pedro.morin1980@gmail.com>
2018-01-27 13:14:15 -0600

    reformat code

--- auth/strategies.js
+++ auth/strategies.js
@@ -1,8 +1,7 @@
 const passport = require('passport');
 const {BasicStrategy} = require('passport-http');
 const {
-    // Assigns the Strategy export to the name JwtStrategy using object
-    // destructuring
+    // Assigns the Strategy export to the name JwtStrategy using object destructuring
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
     Strategy: JwtStrategy,
     ExtractJwt
