c7d61775ee3e87c416844acfa02b8219f4ed59eb
pedrom40 <pedro.morin1980@gmail.com>
2018-02-02 00:27:18 -0600

    make service calls for carousel content for both main apps

--- server.js
+++ server.js
@@ -18,8 +18,14 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/home', (req, res) => {
+// sleep delay for all
+app.use(function (req, res, next) {
   sleep(2000);
+  next();
+});
+
+
+app.get('/home', (req, res) => {
 
   if (Math.random() < 0.25) {
     res.status(500).send({ error: 'Something went wrong when fetching home content...' })
@@ -30,7 +36,6 @@ app.get('/home', (req, res) => {
 });
 
 app.get('/paycheck-planner', (req, res) => {
-  sleep(2000);
 
   if (Math.random() < 0.25) {
     res.status(500).send({ error: 'Something went wrong when fetching paycheck planner content...' })
@@ -39,9 +44,31 @@ app.get('/paycheck-planner', (req, res) => {
   res.json({content: `Content for paycheck planner page...`});
 
 });
+app.get('/paycheck-planner/notices', (req, res) => {
+  const notices = [
+    {
+      title: `Notice 1`,
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et accumsan tellus, non fermentum est. In sed dapibus libero, a laoreet tortor.`,
+      button1: `Go to Event`,
+      button2: `Dismiss`
+    },
+    {
+      title: `Notice 2`,
+      description: `Vivamus at quam et risus pharetra laoreet et non tellus. Ut facilisis magna quis erat aliquet hendrerit.`,
+      button1: `Add Event`,
+      button2: `Dismiss`
+    },
+    {
+      title: `Notice 3`,
+      description: `Praesent feugiat nisi ut velit iaculis, eget accumsan mauris pulvinar. Vivamus vitae ex congue, faucibus diam in, volutpat nulla.`,
+      button1: `Go to Event`,
+      button2: `Email Us`
+    }
+  ];
+  res.json({notices});
+});
 
 app.get('/savings-booster', (req, res) => {
-  sleep(2000);
 
   if (Math.random() < 0.25) {
     res.status(500).send({ error: 'Something went wrong when fetching savings booster content...' })
@@ -50,6 +77,30 @@ app.get('/savings-booster', (req, res) => {
   res.json({content: `Content for savings booster page...`});
 
 });
+app.get('/savings-booster/messages', (req, res) => {
+  const messages = [
+    {
+      title: `Message 1`,
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et accumsan tellus, non fermentum est. In sed dapibus libero, a laoreet tortor.`,
+      button1: `Read More...`,
+      button2: `Dismiss`
+    },
+    {
+      title: `Message 2`,
+      description: `Vivamus at quam et risus pharetra laoreet et non tellus. Ut facilisis magna quis erat aliquet hendrerit.`,
+      button1: `Turn On`,
+      button2: `Dismiss`
+    },
+    {
+      title: `Message 3`,
+      description: `Praesent feugiat nisi ut velit iaculis, eget accumsan mauris pulvinar. Vivamus vitae ex congue, faucibus diam in, volutpat nulla.`,
+      button1: `Yes Please!`,
+      button2: `No Thanks`
+    }
+  ];
+  res.json({messages});
+});
+
 
 app.listen(8080, () => {
   console.log(`Your app is listening on port 8080`);
