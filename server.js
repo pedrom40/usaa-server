f50d7e2ce56e35b37b1a73ad6f09945bf72f099b
pedrom40 <pedro.morin1980@gmail.com>
2018-02-01 23:04:02 -0600

    create all the mock services for each main component

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-require('dotenv').config();
-const bodyParser = require('body-parser');
+const sleep = require('./helpers');
 const express = require('express');
-const mongoose = require('mongoose');
 const morgan = require('morgan');
-const passport = require('passport');
 
-const {router: usersRouter} = require('./users');
-const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
-
-mongoose.Promise = global.Promise;
-
-const {PORT, DATABASE_URL} = require('./config');
 const app = express();
 
 // Logging
@@ -18,71 +9,48 @@ app.use(morgan('common'));
 
 // CORS
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.send(204);
-    }
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
 });
 
-app.use(passport.initialize());
-passport.use(basicStrategy);
-passport.use(jwtStrategy);
+app.get('/home', (req, res) => {
+  sleep(2000);
 
-app.use('/api/users/', usersRouter);
-app.use('/api/auth/', authRouter);
+  if (Math.random() < 0.25) {
+    res.status(500).send({ error: 'Something went wrong when fetching home content...' })
+  }
 
-// A protected endpoint which needs a valid JWT to access it
-app.get('/api/protected', passport.authenticate('jwt', {session: false}),
-    (req, res) => {
-        return res.json({data: 'rosebud'});
-    }
-);
+  res.json({content: `Content for home page...`});
 
-app.use('*', (req, res) => {
-    return res.status(404).json({message: 'Not Found'});
 });
 
-// Referenced by both runServer and closeServer. closeServer assumes runServer has run and set `server` to a server object
-let server;
+app.get('/paycheck-planner', (req, res) => {
+  sleep(2000);
 
-function runServer() {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(DATABASE_URL, err => {
-            if (err) {
-                return reject(err);
-            }
-            server = app
-                .listen(PORT, () => {
-                    console.log(`Your app is listening on port ${PORT}`);
-                    resolve();
-                })
-                .on('error', err => {
-                    mongoose.disconnect();
-                    reject(err);
-                });
-        });
-    });
-}
+  if (Math.random() < 0.25) {
+    res.status(500).send({ error: 'Something went wrong when fetching paycheck planner content...' })
+  }
 
-function closeServer() {
-    return mongoose.disconnect().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing server');
-            server.close(err => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
-    });
-}
+  res.json({content: `Content for paycheck planner page...`});
 
-if (require.main === module) {
-    runServer().catch(err => console.error(err));
-}
+});
 
-module.exports = {app, runServer, closeServer};
+app.get('/savings-booster', (req, res) => {
+  sleep(2000);
+
+  if (Math.random() < 0.25) {
+    res.status(500).send({ error: 'Something went wrong when fetching savings booster content...' })
+  }
+
+  res.json({content: `Content for savings booster page...`});
+
+});
+
+app.listen(8080, () => {
+  console.log(`Your app is listening on port 8080`);
+});
